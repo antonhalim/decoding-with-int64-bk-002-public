6b24237f6998c79ac237a9f085c0099dd61f71ad
axton21 <axton_n@yahoo.com>
2015-05-10 16:01:12 -0400

    done

--- lib/encode_decode.rb
+++ lib/encode_decode.rb
@@ -1,7 +1,21 @@
+require 'pry'
 def encode_int64(message)
-  # code this method first
+  return "The word should only contain letters in acdegilmnoprstuw" if !/[bfhjkqvxyz]/.match(message).nil?
+  string = "acdegilmnoprstuw"
+  result = 7
+  message.split("").each do |char|
+    result = (result*37) + string.index(char)
+  end
+  result
 end
 
 def decode_int64(encoded_message)
-  # code this second
+  string = "acdegilmnoprstuw"
+  result = ""
+  until encoded_message - 7 == 0 do
+    index = encoded_message % 37
+    result  << string[index]
+    encoded_message = encoded_message/37
+  end
+  return result.reverse
 end
